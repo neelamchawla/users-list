011cfcd1bc7d40c327768b302a1cd527dd92640d
unknown <neelamchawla55@gmail.com>
2020-03-31 18:10:48 +0530

    Updates

---
+++ precache-manifest.e52d4996d42a979157ceab7cd4ac1ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265d72943ea8b94a2ec370ec87296978",
+    "url": "/neelamchawla/users-list.git/index.html"
+  },
+  {
+    "revision": "73327691599aa253aabb",
+    "url": "/neelamchawla/users-list.git/static/css/main.75a5f6ce.chunk.css"
+  },
+  {
+    "revision": "216a2ae6f5b64368a64c",
+    "url": "/neelamchawla/users-list.git/static/js/2.5132d7ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neelamchawla/users-list.git/static/js/2.5132d7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73327691599aa253aabb",
+    "url": "/neelamchawla/users-list.git/static/js/main.1f8c8e02.chunk.js"
+  },
+  {
+    "revision": "08098e4dddfab5f2e5a7",
+    "url": "/neelamchawla/users-list.git/static/js/runtime-main.77b05165.js"
+  }
+]);
